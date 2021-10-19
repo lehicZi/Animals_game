03f460a7ac2690702b2fe0f29c04e1daa9934c8d
Lolo <loic.fornelli@gmail.com>
2021-10-19 20:47:16 +0200

    Meilleures conditions pour les fights

--- src/Fight.java
+++ src/Fight.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
 public class Fight {
-    public static Animal animalFight (Animal animal1, Animal animal2){
+    public static Animal animalFight (Animal animal1, Animal animal2) {
         String effectiveAttribute = AskAttribute();
-        if (animal1.GetAttribute(effectiveAttribute) > animal2.GetAttribute(effectiveAttribute)){
+        if (animal1.GetAttribute(effectiveAttribute) > animal2.GetAttribute(effectiveAttribute)) {
             return animal1;
-        }
-        else {
+        } else if (animal1.GetAttribute(effectiveAttribute) < animal2.GetAttribute(effectiveAttribute)) {
             return animal2;
-        }
-    }
+        } else {
+            if (animal1.getRareté() > animal2.getRareté()){
+                return animal1;
+            }
+            else if (animal1.getRareté() < animal2.getRareté()) {
+                return animal2;
+            }
+            else {
+                return animal1; }
+            }
+            }
     public static String AskAttribute (){
         Scanner userChoice = new Scanner(System.in);
         System.out.println("Please enter the chose the attribute (1 : poids, 2 : longueur, 3 : longévité, 4 : Gestation/incubation)");
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] arg){
     All_animals animaux = new All_animals();
-        System.out.println(Fight.animalFight(animaux.opossum,animaux.gecko).getNom());
+        System.out.println(Fight.animalFight(animaux.test2,animaux.test).getNom());
     }
 }
